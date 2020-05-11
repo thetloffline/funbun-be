f181366891dc92ffb4bbf8fe2c13cc5dfe262c7d
thetloffline <thetloffline@users.noreply.github.com>
2020-05-11 23:35:58 +0300

    added image upload

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,8 +1,22 @@
 const Feedback = require('../models/feedback')
 const success = require('../utils/respond')
+const path = require('path')
 
 exports.create = async (req, res) => {
   try {
+    if (req.files) {
+      const productImage = req.files['files[0]']
+      const saltedProductImage = (Math.floor(Date.now() / 1000)) + '-' + productImage.name 
+      const filePath = path.join(__dirname, '/../public/images/', saltedProductImage)
+
+      await productImage.mv(filePath, (err) => {
+        if (err) {
+          return console.log(err)
+        }
+        console.log('file moved to ', filePath)
+      })
+      req.body.productImage = saltedProductImage
+    }
     const feedback = await Feedback.create(req.body)
     success(res, feedback)
   } catch (error) {
