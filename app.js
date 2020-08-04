f46392eee9704577ea693289f77664cbd88c27f3
thetloffline <thetloffline@users.noreply.github.com>
2020-08-04 17:04:09 +0300

    update

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(logger('dev'))
 app.use(express.json())
 //app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/api', router)
 
 // catch 404 and forward to error handler
