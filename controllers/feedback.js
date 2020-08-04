cc6714e2daa3a38167483a929337972b01351350
thetloffline <thetloffline@users.noreply.github.com>
2020-08-04 16:46:28 +0300

    added update Taste method

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -6,16 +6,21 @@ exports.create = async (req, res) => {
   try {
     if (req.files) {
       const productImage = req.files['files[0]']
-      const saltedProductImage = (Math.floor(Date.now() / 1000)) + '-' + productImage.name 
-      const filePath = path.join(__dirname, '/../public/images/', saltedProductImage)
+      const timestampedProductImage = (Math.floor(Date.now() / 1000)) + '-' + productImage.name 
+
+      const filePath = path.join(__dirname, '/../public/images/', timestampedProductImage)
 
       await productImage.mv(filePath, (err) => {
         if (err) {
           return console.log(err)
         }
-        console.log('file moved to ', filePath)
+
+        console.log('file moved to ', req.hostname + '7' + filePath)
       })
-      req.body.productImage = saltedProductImage
+
+      req.body.productImage = timestampedProductImage
+    } else if (!req.body.productImage) {
+      console.log('no image was added to the request')
     }
     const feedback = await Feedback.create(req.body)
     success(res, feedback)
@@ -30,5 +35,18 @@ exports.allFeedback = async (req, res) => {
     success(res, result)
   } catch (error) {
     console.log('error getting all feedback :: ', error)
+  }
+}
+
+exports.updateTaste = async (req,res) => {
+  try {  
+    const result = await Feedback.findOneAndUpdate( 
+      {_id: req.params.id},
+      { $set : { taste: req.body.taste } },
+      { new : true }
+      )
+    success(res, result)
+  } catch (error) {
+   console.log(error) 
   }
 }
